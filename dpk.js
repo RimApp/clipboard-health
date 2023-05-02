0c44032acf54ebf913bd891fcefeaa46da2460dd
rimapp <rimani.sami@gmail.com>
2023-05-03 01:58:25 +0200

    simplified version

--- dpk.js
+++ dpk.js
@@ -1,28 +1,15 @@
 const crypto = require("crypto");
 
-exports.deterministicPartitionKey = (event) => {
-  const TRIVIAL_PARTITION_KEY = "0";
-  const MAX_PARTITION_KEY_LENGTH = 256;
-  let candidate;
+exports.deterministicPartitionKey = event => {
+	const DEFAULT_PARTITION_KEY = "0";
+	const MAX_PARTITION_KEY_LENGTH = 256;
 
-  if (event) {
-    if (event.partitionKey) {
-      candidate = event.partitionKey;
-    } else {
-      const data = JSON.stringify(event);
-      candidate = crypto.createHash("sha3-512").update(data).digest("hex");
-    }
-  }
-
-  if (candidate) {
-    if (typeof candidate !== "string") {
-      candidate = JSON.stringify(candidate);
-    }
-  } else {
-    candidate = TRIVIAL_PARTITION_KEY;
-  }
-  if (candidate.length > MAX_PARTITION_KEY_LENGTH) {
-    candidate = crypto.createHash("sha3-512").update(candidate).digest("hex");
-  }
-  return candidate;
+	const partitionKey = event
+		? event.partitionKey || crypto.createHash("sha3-512").update(JSON.stringify(event)).digest("hex")
+		: DEFAULT_PARTITION_KEY;
+	return typeof partitionKey !== "string"
+		? JSON.stringify(partitionKey)
+		: partitionKey.length > MAX_PARTITION_KEY_LENGTH
+		? crypto.createHash("sha3-512").update(partitionKey).digest("hex")
+		: partitionKey;
 };
